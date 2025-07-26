b20b37661870384ff9bcd25ddf256c55a1511b50
Vicente Gonzalez <120504099+goev10@users.noreply.github.com>
2025-07-25 20:36:48 -0600

    Adding log to E AND E application

--- src/main/java/com/web/back/controllers/EntryAndExitController.java
+++ src/main/java/com/web/back/controllers/EntryAndExitController.java
@@ -9,6 +9,8 @@ import com.web.back.services.EmployeeService;
 import com.web.back.services.EntryAndExitService;
 import com.web.back.services.JwtService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,8 @@ import java.util.List;
 @RequestMapping("/entrada-salida")
 @Tag(name = "Entrada y salida")
 public class EntryAndExitController {
+    private static final Logger logger = LoggerFactory.getLogger(EntryAndExitController.class);
+
     private final EntryAndExitService entryAndExitService;
     private final EmployeeService employeeService;
     private final JwtService jwtService;
@@ -28,8 +32,10 @@ public class EntryAndExitController {
     }
 
     @PostMapping
-    public CustomResponse<Void> getEmployeesEvaluations(@RequestHeader("Authorization") String bearerToken, @RequestBody List<EntryExitMarkRequest> entryAndExitMarks) {
+    public CustomResponse<Void> registerEntryAndExitMarks(@RequestHeader("Authorization") String bearerToken, @RequestBody List<EntryExitMarkRequest> entryAndExitMarks) {
+        logger.info("POST /entrada-salida called with body: {}", entryAndExitMarks);
         if (!PermissionsFilter.hasPermission(jwtService.getPermissionsFromToken(bearerToken), PermissionsEnum.CARGAR_MARCAJES)) {
+            logger.error("No tienes permisos para realizar esta acción");
             return new CustomResponse<Void>().forbidden("No tienes permisos para realizar esta acción");
         }
 
--- src/main/java/com/web/back/model/requests/EntryExitMarkRequest.java
+++ src/main/java/com/web/back/model/requests/EntryExitMarkRequest.java
@@ -1,8 +1,22 @@
 package com.web.back.model.requests;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public record EntryExitMarkRequest(
         String empleado,
         String fecha,
         String hora,
         String tipo) {
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+        return "EntryExitMarkRequest{" +
+                "empleado='" + empleado + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", hora='" + hora + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 }
